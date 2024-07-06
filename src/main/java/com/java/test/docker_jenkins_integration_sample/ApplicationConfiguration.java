872e99325d853986ce4b129832efaa268307659c
sonu <sonu0829>
2024-07-06 15:28:24 +0530

    add all files

---
+++ src/main/java/com/java/test/docker_jenkins_integration_sample/ApplicationConfiguration.java
@@ -0,0 +1,8 @@
+package com.java.test.docker_jenkins_integration_sample;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ApplicationConfiguration {
+
+}
